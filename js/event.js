a44da2619aff10df94a8ce69d6e970f02554436b
ummeFA <ummefatema33@gmail.com>
2023-02-18 03:19:41 +0600

    triangle successful update

--- js/event.js
+++ js/event.js
@@ -1,24 +1,16 @@
-document.getElementById('triangle-area').addEventListener('click',function(){
+
+// for triangle
+document.getElementById('triangle-value').addEventListener('click',function(){
     const base = document.getElementById('base-value');
     const height = document.getElementById('height-value');
 
-    const finalValue = document.getElementById('final-output');
-    const finalValueString = finalValue.innerText;
-    const finalArea = parseFloat(finalValueString);
 
     const baseValue = base.value;
     const heightValue = height.value;
     const areaTriangle= 0.5*parseFloat(baseValue)*parseFloat(heightValue);
 
-    finalArea.innerText = areaTriangle;
+    document.getElementById('final-output').innerText = areaTriangle;
+    document.getElementById('geometric-shape').innerText= 'Triangle';
     
 })
-// document.getElementById('ellipse-area').addEventListener('click',function(){
-//     const inputA = document.getElementById('input-a').value;
-//     const inputANew =inputA.value;
-//     console.log(inputANew); 
-//     // const inputB = document.getElementById('input-b').value;
-//     // const areaEllipse = 3.1416*parseFloat(inputA)*parseFloat(inputB);
-//     // document.getElementById('final-output').value=areaEllipse;
 
-// })
