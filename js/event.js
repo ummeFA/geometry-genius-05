4c0006dcca1dad33e9f8bd7b352d011da5b7413a
ummeFA <ummefatema33@gmail.com>
2023-02-18 00:04:18 +0600

    blog added

--- js/event.js
+++ js/event.js
@@ -1,1 +1,7 @@
-document.getElementById
+document.getElementById('ellipse-area').addEventListener('click',function(){
+    const inputA = document.getElementById('input-a').value;
+    const inputB = document.getElementById('input-b').value;
+    const areaEllipse = 3.1416*parseFloat(inputA)*parseFloat(inputB);
+    document.getElementById('final-output').value=areaEllipse;
+
+})
