4f6b9733982197b174c5133fc4a54275ac7abb25
ummeFA <ummefatema33@gmail.com>
2023-02-18 09:26:53 +0600

    rectangle added

--- js/event.js
+++ js/event.js
@@ -1,4 +1,4 @@
-
+// let finalOutput;
 // for triangle
 document.getElementById('triangle-value').addEventListener('click',function(){
     const base = document.getElementById('base-value');
@@ -11,6 +11,30 @@ document.getElementById('triangle-value').addEventListener('click',function(){
 
     document.getElementById('final-output').innerText = areaTriangle;
     document.getElementById('geometric-shape').innerText= 'Triangle';
+    
+})
+
+//for rectangle
+document.getElementById('rectangle-value').addEventListener('click',function(){
+    const width = document.getElementById('width-value');
+    const height = document.getElementById('height-value');
+
+
+    const widthValue = width.value;
+    const heightValue = height.value;
+    const areaRectangle= parseFloat(widthValue)*parseFloat(heightValue);
+
+    const previousFinalOutput = document.getElementById('final-output');
+    const finalOutputString = previousFinalOutput.innerText;
+    const currentOutput= finalOutputString +'\n'+areaRectangle;
+    previousFinalOutput.innerText = currentOutput;
+
+    const previousGeometry = document.getElementById('geometric-shape').innerText;
+    document.getElementById('geometric-shape').innerText= previousGeometry+ ' Rectangle';
+    
+
+    document.getElementById('final-output').innerText = previousFinalOutput.innerText;
+
     
 })
 
